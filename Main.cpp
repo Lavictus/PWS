2ad74d7e066d8767258d29a0470afd5e62cf5188
Lavictus <markb@baaijbreda.nl>
2017-01-11 20:50:41 +0100

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,82 @@
+// Made by Mark Baaij
+// NHTV Programming Entry Assingment
+// For additional information and sources see the "Read Me" file
+
+//including Header Files -----------------------------------------------------------------------------------------------------------//
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+
+
+// Main function -------------------------------------------------------------------------------------------------------------------//
+int main()
+{
+	//Render Screen -------------------------------------------------------------------------------------------------------------------//
+	const int ScreenWidth = 1300;
+	const int ScreenHeigt = ScreenWidth / 1.5;
+
+	sf::RenderWindow window(sf::VideoMode(ScreenWidth, ScreenHeigt), "Catch the Bomb", sf::Style::Close);
+
+	//Create Background ------------------------------------------------------------------------------------------------------------//
+
+	sf::Texture background;
+	sf::Sprite Landscape;
+
+	if (!background.loadFromFile("BackgroundBlue2.png"))
+	{
+		std::cout << "Could not load Background!" << std::endl;
+	}
+
+	Landscape.setTexture(background);
+
+	// Set Font -----------------------------------------------------------------------------------------------------------------------//
+
+	sf::Font font;
+	if (!font.loadFromFile("Shadows of Security.ttf"))
+	{
+		std::cout << "Could not load Font!" << std::endl;
+	}
+
+	sf::Text text;
+	text.setFont(font);
+	text.setString("Catch The Bomb");
+	text.setCharacterSize(150);
+	text.setPosition(235, 250); 
+	text.setOutlineThickness(1);
+	text.setOutlineColor(sf::Color::Blue);
+
+	sf::Text undertext;
+	undertext.setFont(font);
+	undertext.setString("By Lavictus");
+	undertext.setCharacterSize(100);
+	undertext.setPosition(450, 430);
+	undertext.setOutlineThickness(1);
+	undertext.setOutlineColor(sf::Color::Blue);
+
+	
+	
+	
+	// Game Loop ----------------------------------------------------------------------------------------------------------------------//
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+	//Draw Window ---------------------------------------------------------------------------------------------------------------------//
+		window.clear();
+		window.draw(Landscape);
+		window.draw(text);
+		window.draw(undertext);
+		window.display();
+
+	}
+
+
+	//Return Exit Value ---------------------------------------------------------------------------------------------------------------//
+	return 0;
+}
